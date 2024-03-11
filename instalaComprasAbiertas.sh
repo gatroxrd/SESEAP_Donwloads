9859aefca2470e326108f7e4eab81f3e5bf46c81
Gabriel Rodríguez Domínguez <52460428+gatroxrd@users.noreply.github.com>
2024-03-11 15:23:25 -0600

    Add files via upload

--- instalaComprasAbiertas.sh
+++ instalaComprasAbiertas.sh
@@ -82,6 +82,38 @@ _NodeJs()
 
 }
 
+_NodeJs2()
+{
+		sudo apt-get remove nodejs npm
+		sudo rm /usr/local/bin/node
+		sudo rm /usr/local/bin/npm
+		sudo rm -rf /etc/nodejs
+		sudo rm -rf ~/.npm
+		sudo rm -rf ~/.cache/node-gyp
+		nvm uninstall
+		#------------------------------------------
+                #Instalando NodeJs
+                clear
+                sudo curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+                sudo apt update
+                sudo apt install nodejs
+                sudo wget https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.gz
+                sudo tar -xf node-v18.12.1-linux-x64.tar.gz
+                sudo sudo mv node-v18.12.1-linux-x64/* /usr/local/bin
+                #------------------
+                #sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh 
+                ruta_script=$(readlink -f “$0”)
+                sudo wget -O  ruta_script  https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh
+                cd "$(dirname "$ruta_script")"
+                sudo chmod +u+x ruta_script
+                sudo ./ruta_script
+                #- - - - - - - - - -
+                nvm install 18.12.1
+                echo -e " \033[33mNodeJs 18 instalado!\033[0m."
+                #echo "NodeJs 18 instalado!"
+
+}
+
 _Nginx()
 {
 
@@ -447,7 +479,7 @@ prerrequisitosInstalacion()
 						_MongoDB
 						echo -e "\e[33mPaso Mongo DB terminado.\e[0m"
 						echo -e "\e[33mIniciando NodeJs\e[0m"
-						_NodeJs
+						_NodeJs2
 						echo -e "\e[33mPaso NodeJs terminado.\e[0m"
 						echo -e "\e[33mIniciando Nginx\e[0m"
 						_Nginx
@@ -1052,12 +1084,12 @@ paso10()
 
 paso11()
 {
-	#npm install -g npm-force-update
-	sudo apt-get remove --auto-remove
-	sudo apt update
-	#sudo apt install --reinstall nodejs
-	#sudo apt clean
-	sudo apt install npm
+			#npm install -g npm-force-update
+	#sudo apt-get remove --auto-remove
+	#sudo apt update
+			#sudo apt install --reinstall nodejs
+			#sudo apt clean
+	#sudo apt install npm
 	#Instalación de Object Relational Mapping del módulo de Infraestructura
 	cd /var/www/html/contratacionesabiertas
 	cd captura
